4492dc17d9d2a4d0952525082f2df028b89250fe
J. Smith <6228270+TechNoirBreakfastClub@users.noreply.github.com>
2019-10-18 14:50:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const hostname = process.ENV.NODE_HOST || "127.0.0.1";
+const port = process.ENV.POST || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
